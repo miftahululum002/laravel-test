949894e3ffb4350c0fa2ffd57af154e5ce4e69d7
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-07-10 23:04:07 +0700

    ubahan

---
+++ resources/views/work.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
+
+    <title>Work Transaction</title>
+  </head>
+  <body>
+    <h1>Operator Work Transaction</h1>
+    <script src="{{asset('assets')}}/bootstrap/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
